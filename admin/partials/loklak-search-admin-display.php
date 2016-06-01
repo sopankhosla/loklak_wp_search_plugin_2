a6802283bddd9271e28fd46a96d9020cfda6bf53
skhosla <skhosla@adobe.com>
2016-06-01 09:55:59 +0530

    Adds admin support and README files

---
+++ admin/partials/loklak-search-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       https://in.linkedin.com/in/sopan-khosla-56319999
+ * @since      1.0.0
+ *
+ * @package    Loklak_Search
+ * @subpackage Loklak_Search/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
